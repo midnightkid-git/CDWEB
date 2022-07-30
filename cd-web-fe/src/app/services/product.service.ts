19c27ac4e792087f06bd646c228cbb3f884e8833
18130031@st.hcmuaf.edu.vn <18130031@st.hcmuaf.edu.vn>
2022-07-30 21:32:04 +0700

    add getProdcuts and getCategories in Shop User page

--- cd-web-fe/src/app/services/product.service.ts
+++ cd-web-fe/src/app/services/product.service.ts
@@ -1,24 +1,25 @@
 import { Injectable } from '@angular/core';
-import { HttpHeaders } from '@angular/common/http';
-const httpOptions = {
-  headers: new HttpHeaders({
-    'Content-Type':  'application/json',
-    Authorization: 'my-auth-token'
-  })
-};
+import { environment } from 'src/environments/environment';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/json',
+      Authorization: 'my-auth-token'
+    })
+  };
 
- 
-  
-  constructor() { }
+  constructor(private http: HttpClient) {}
 
-  
-
-  getProducts() {
+  getCategories() {
+    return this.http.get<any>(`${environment.apiUrl}/user/category/no-token`, this.httpOptions);
   }
 
-  
+  getProducts() {
+    return this.http.get<any>(`${environment.apiUrl}/user/product/no-token/1/1`, this.httpOptions);
+  }
 }
