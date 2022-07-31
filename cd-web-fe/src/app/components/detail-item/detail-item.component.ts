a35e9571c8377f3e9d46d80909da070a6cca243b
18130031@st.hcmuaf.edu.vn <18130031@st.hcmuaf.edu.vn>
2022-07-31 21:59:58 +0700

    Add details product page functionality

--- cd-web-fe/src/app/components/detail-item/detail-item.component.ts
+++ cd-web-fe/src/app/components/detail-item/detail-item.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { MessageService } from 'primeng/api';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-detail-item',
@@ -9,10 +11,10 @@ import { ActivatedRoute } from '@angular/router';
 export class DetailItemComponent implements OnInit {
 
   public item: any;
-  public selectedSize: any
+  public selectedSize: any;
   public totalItem: number = 0;
 
-  constructor(private activateeRoute: ActivatedRoute) { }
+  constructor(private activateeRoute: ActivatedRoute, private productService: ProductService, private messageService: MessageService) { }
 
   ngOnInit(): void {
     this.fetchDataByRoute()
@@ -20,9 +22,27 @@ export class DetailItemComponent implements OnInit {
 
   fetchDataByRoute() {
     this.activateeRoute.paramMap.subscribe(_param => {
-      console.log(_param.get("id"))
-      this.initMockData();
+      this.productService.getProduct(_param.get("id")).subscribe((res) => {
+        console.log('Checking res: ', res);
+        this.item = res.data
+        this.item.totalItem = this.item.listSizes.reduce((a: any, b: any) => {
+          return a + b['quantity'];
+        }, 0);
+      });
+      // this.initMockData();
     })
+  }
+
+  addToCart(): void {
+    const payload = { productId: this.item.id, size: this.selectedSize.sizeId, quantity: this.selectedSize.quantity };
+    this.productService.addToCart(payload).subscribe((res) => {
+      this.messageService.add({
+        severity: 'success',
+        summary: 'Successful',
+        detail: res.message,
+        life: 3000,
+      });
+    });
   }
 
   initMockData() {
@@ -52,5 +72,4 @@ export class DetailItemComponent implements OnInit {
       ]
     }
   }
-
 }
