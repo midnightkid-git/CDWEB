3fa206f4ead0fc6428a14bc7675b8ca560aa90bb
DatMHP <18130031@st.hcmuaf.edu.vn>
2022-08-01 23:59:20 +0700

    change sizeId to size_id

--- cd-web-fe/src/app/components/detail-item/detail-item.component.ts
+++ cd-web-fe/src/app/components/detail-item/detail-item.component.ts
@@ -6,35 +6,42 @@ import { ProductService } from 'src/app/services/product.service';
 @Component({
   selector: 'app-detail-item',
   templateUrl: './detail-item.component.html',
-  styleUrls: ['./detail-item.component.scss']
+  styleUrls: ['./detail-item.component.scss'],
 })
 export class DetailItemComponent implements OnInit {
-
   public item: any;
   public selectedSize: any;
   public totalItem: number = 0;
 
-  constructor(private activateeRoute: ActivatedRoute, private productService: ProductService, private messageService: MessageService) { }
+  constructor(
+    private activateeRoute: ActivatedRoute,
+    private productService: ProductService,
+    private messageService: MessageService
+  ) {}
 
   ngOnInit(): void {
-    this.fetchDataByRoute()
+    this.fetchDataByRoute();
   }
 
   fetchDataByRoute() {
-    this.activateeRoute.paramMap.subscribe(_param => {
-      this.productService.getProduct(_param.get("id")).subscribe((res) => {
+    this.activateeRoute.paramMap.subscribe((_param) => {
+      this.productService.getProduct(_param.get('id')).subscribe((res) => {
         console.log('Checking res: ', res);
-        this.item = res.data
+        this.item = res.data;
         this.item.totalItem = this.item.listSizes.reduce((a: any, b: any) => {
           return a + b['quantity'];
         }, 0);
       });
       // this.initMockData();
-    })
+    });
   }
 
   addToCart(): void {
-    const payload = { productId: this.item.id, size: this.selectedSize.sizeId, quantity: this.selectedSize.quantity };
+    const payload = {
+      productId: this.item.id,
+      size: this.selectedSize.size_id,
+      quantity: this.selectedSize.quantity,
+    };
     this.productService.addToCart(payload).subscribe((res) => {
       this.messageService.add({
         severity: 'success',
@@ -47,29 +54,29 @@ export class DetailItemComponent implements OnInit {
 
   initMockData() {
     this.item = {
-      productId: "asbas",
+      productId: 'asbas',
       productLabel: "Tommy Shelby's Jacket",
       price: 75,
       size: [
         {
-          sizeType: "XL",
-          totalItem: 1
+          sizeType: 'XL',
+          totalItem: 1,
         },
         {
-          sizeType: "M",
-          totalItem: 0
+          sizeType: 'M',
+          totalItem: 0,
         },
         {
-          sizeType: "L",
-          totalItem: 5
-        }
+          sizeType: 'L',
+          totalItem: 5,
+        },
       ],
       totalItem: 6,
-      categoryId: "asgas",
-      categoryLabel: "Vest",
+      categoryId: 'asgas',
+      categoryLabel: 'Vest',
       thumbnails: [
-        "https://cdn.shopify.com/s/files/1/0613/8662/1177/products/Cillian-Murphy-Peaky-Blinders-Thomas-Shelby-Coat_1024x1024@2x.jpg"
-      ]
-    }
+        'https://cdn.shopify.com/s/files/1/0613/8662/1177/products/Cillian-Murphy-Peaky-Blinders-Thomas-Shelby-Coat_1024x1024@2x.jpg',
+      ],
+    };
   }
 }
